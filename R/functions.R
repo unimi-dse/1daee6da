aedcf056935cfe1318916a73b2d8c6fb35903b9f
Filip Vitez <filip.vitez@lunicore.se>
2020-02-06 08:36:52 +0100

    updating package structure and roxygen2 documentation according to feedback from Prof. Guidotti

--- R/functions.R
+++ R/functions.R
@@ -9,8 +9,11 @@
 #' @title Import Data
 #'
 #' @description This function reads the data from a csv-file and returns it as a DataFrame-object.
-#' @usage To use this function by itself, store the DF in a variable by the following command: > df = import_data()
-#' @importFrom readr
+#' @usage To use this function by itself, store the DF in a variable.
+#' @examples
+#' df = import_data()
+#' @import readr
+#' @export
 import_data <- function() {
   df <- read_csv(system.file("extdata", "train.csv", package="titanicanalysis"))
   return(df)
@@ -21,27 +24,39 @@ import_data <- function() {
 #' @title Passenger Age Mean
 #'
 #' @description This function returns the mean age of the passengers.
-#' @usage To use this function by itself, you need to input the Titanic DF. The DF can be stored as a variable by using the function import_data(). You can then call the use the function with the command: > age_mean(df)
+#' @usage To use this function by itself, you need to input the Titanic DF. The DF can be stored as a variable by using the function import_data().
+#' @param df The Titanic dataframe. Raw data as csv from Kaggle, converted to a R dataframe with the function read_csv.
+#' @examples
+#' age_mean(df)
+#' @export
 age_mean <- function(df){
-  sprintf("The mean age of all the passengers was: %f", mean(df[['Age']][complete.cases(df[['Age']])]))
+  sprintf("The mean age of all the passengers was: %f", mean(df[['Age']], na.rm = TRUE))
 }
 
 # Calculate mean age of survivors
 #' @title Survivor Age Mean
 #'
 #' @description This function returns the mean age of the survivors.
-#' @usage To use this function by itself, you need to input the Titanic DF. The DF can be stored as a variable by using the function import_data(). You can then call the use the function with the command: > survivor_age_mean(df)
+#' @usage To use this function by itself, you need to input the Titanic DF. The DF can be stored as a variable by using the function import_data().
+#' @param df The Titanic dataframe. Raw data as csv from Kaggle, converted to a R dataframe with the function read_csv.
+#' @examples
+#' survivor_age_mean(df)
+#' @export
 survivor_age_mean <- function(df){
   i <- df[['Survived']] == 1
-  sprintf("The mean age of the survivors was %f:", mean(df[['Age']][i][complete.cases(df[['Age']][i])]))
+  sprintf("The mean age of the survivors was %f:", mean(df[['Age']][i], na.rm = TRUE))
 }
 
 # Scatter plot of passengers based on age (x-axis) and gender (color)
 #' @title Plot Passengers
 #'
 #' @description This function plots a scatter plot of the passengers, with the binary feature "Survived" on the y axis, and integer feature "Age" on the x-axis. The gender of each passenger is represented as colors. It is not included in the main-func.
-#' @usage To use this function by itself, you need to input the Titanic DF as an argument for the function. The DF can be stored as a variable by using the function import_data(). You can then call the use the function with the command: > survivor_age_mean(df)
-#' @importFrom plotly
+#' @usage To use this function by itself, you need to input the Titanic DF as an argument for the function. The DF can be stored as a variable by using the function import_data().
+#' @param df The Titanic dataframe. Raw data as csv from Kaggle, converted to a R dataframe with the function read_csv.
+#' @examples
+#' survivor_age_mean(df)
+#' @import plotly
+#' @export
 plot_passengers <- function(df){
   p <- plot_ly(data=df, x=~Age, y=~Survived, color=~Sex, colors="Set1", type="scatter")
   p
@@ -51,8 +66,11 @@ plot_passengers <- function(df){
 #' @title Main function
 #'
 #' @description This function runs the various functions of the package in the correct order.
-#' @usage Run the function by calling it without arguments: > main_func()
-#' @importFrom DataExplorer
+#' @usage Run the function by calling it without arguments.
+#' @examples
+#' main_func()
+#' @import DataExplorer
+#' @export
 main_func <- function(){
   df = import_data()
   create_report(df)
